3677eab632472122ddf3046fe9b060693fc60746
ManaveAngel <156461255+ManaveAngel@users.noreply.github.com>
2024-01-15 13:45:49 -0500

    added a new repositary
    made some changes

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 #file from zen of python
 print("Although that way may not be obvious at first unless you're Dutch.")
 print("Special cases aren't special enough to break the rules.")
+print("Lokinh back is not a good idea.")
